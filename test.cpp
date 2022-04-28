7bbebfac743c8ce2e05bb6da5f8115b0ac73b097
Kacper Horowski <79279375+GhostSiC@users.noreply.github.com>
2022-04-28 22:25:48 +0200

    ex3.02

--- test.cpp
+++ test.cpp
@@ -106,31 +106,6 @@ bool isColliding(const triangle& triangle1, const triangle& triangle2) {
     return 0;
 }
 
-void ex3() {
-
-    std::string str = "   \t  przykladowy string  ";
-
-    std::cout << '|' << str << '|' << '\n';
-
-    for (int i = 0; i < str.length() - 1; i++) {
-        if (isalpha(str.at(i))) {
-            //str = str.substr(i);
-            str.erase(0, i);
-            break;
-        }    
-    }
-
-    for (int i = str.length() - 1; i >= 0 ; i--) {
-        if (isalpha(str.at(i))) {
-            //str = str.substr(0, i+1);
-            str.erase(i+1);
-            break;
-        }
-    }
-    std::cout << '|' << str << '|' << '\n';
-    std::cout << str.length() << '\n';
-}
-
 int main()
 {
     //ex3();
