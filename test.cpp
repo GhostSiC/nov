242ececfd1bf592519a7af29254dcae894efc2bc
Kacper Horowski <79279375+GhostSiC@users.noreply.github.com>
2022-04-28 22:31:41 +0200

    ex3.01

--- test.cpp
+++ test.cpp
@@ -1,20 +1,32 @@
-﻿// test.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
-
+﻿
 #include <iostream>
+
+void ex3() {
+
+    std::string str = "   \t  przykladowy string  ";
+
+    std::cout << '|' << str << '|' << '\n';
+
+    for (int i = 0; i < str.length() - 1; i++) {
+        if (isalpha(str.at(i))) {
+            //str = str.substr(i);
+            str.erase(0, i);
+            break;
+        }    
+    }
+
+    for (int i = str.length() - 1; i >= 0 ; i--) {
+        if (isalpha(str.at(i))) {
+            //str = str.substr(0, i+1);
+            str.erase(i+1);
+            break;
+        }
+    }
+    std::cout << '|' << str << '|' << '\n';
+    std::cout << str.length() << '\n';
+}
 
 int main()
 {
-    std::cout << "test\n";
+    ex3();
 }
-
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
